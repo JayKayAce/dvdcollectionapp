ca5b5063b9845eb4c8532beb565184a86b27afc6
JayKayAce <jesper.kjaer.soerensen@gmail.com>
2019-07-11 15:24:49 +0200

    added posts

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello from Flask"
+    user = {"username": "Jesper"}
+    posts = [
+        {
+            "author":{"username":"John"},
+            "body":"A beautiful day"
+        },
+        {
+            "author":{"username":"Susan"},
+            "body":"Keep on trucking"
+        }
+    ]
+    return render_template('index.html', title = "Home", user=user, posts=posts)
 
