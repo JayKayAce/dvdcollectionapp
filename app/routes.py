9c5c733237ff7200d21ec8d5e32c2887eecc7619
JayKayAce <jesper.kjaer.soerensen@gmail.com>
2019-07-12 13:25:14 +0200

    Added user.last_seen handling

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template
 from flask import flash
 from flask import redirect
@@ -78,3 +80,9 @@ def user(username):
         {"author" : user, "body": "test post #2"}
     ]
     return render_template("user.html", user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
